b7138cd348a4b950c1378d0e3467f7396c417074
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-11 20:52:47 +0100

    slide change motion

--- src/components/MainSlider.tsx
+++ src/components/MainSlider.tsx
@@ -25,9 +25,20 @@ export default function MainSlider({slides}:any){
     return (
         <>
             <div className="mr-[340px]">
-                <Link to='/' className="block w-[100%] h-[505px]">
-                    <img className="w-[100%] h-[100%]" src='/pexel8.jpg' alt="mainslider"/>
-                </Link>
+
+                {
+                    slides.filter((slide:string,i:number)=>{
+                        if(i===currentslide){
+                            return slide
+                        }else{
+                            return null
+                        }
+                    }).map((slide:string,i:number)=>{
+                        return  <Link key={i} to='/' className="block w-[100%] h-[505px]">
+                                    <img className="w-[100%] h-[100%]" src={slide} alt="mainslider"/>
+                                </Link>
+                    })
+                }
                 <div className="flex items-center justify-between mt-[-279px]">
                 <div><i className="fa fa-arrow-left p-4 bg-black border border-txtPrimary rounded-md text-txtPrimary" onClick={()=>prevslide()}/></div>
                 <div><i className="fa fa-arrow-right p-4 bg-black border border-txtPrimary rounded-md text-txtPrimary" onClick={()=>nextslide()}/></div>
