3b32a9d9fa18ed2be9f748330d1838cca4801a50
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-13 23:14:59 +0100

    filter fixed

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -6,12 +6,12 @@ export default function Filter({slides}:any){
     return(
         <>
         <div className="flex-1">
-                <div>
+                <div className="w-[46vw]">
                     <input type='search' placeholder="Search movies, series, novellas ..." className="outline-none 
                     text-bgPrimary placeholder-txtSecondary px-5 py-4 w-full bg-brTertiary rounded-full"/>
                     <i className="fa fa-search ml-[-30px] text-bgSecondary"/>
                 </div>
-                <div className="z-20 absolute w-[42vw] mt-2 rounded-xl bg-brSecondary px-2">
+                {/* <div className="z-20 absolute w-[46vw] mt-2 rounded-xl bg-brSecondary px-2">
                     {slides.map((slide:string,i:number):any=>{
                         return <Link to='/' key={i} className='flex py-2 border-b'>
                                     <div className="w-[60px] h-[80px]">
@@ -24,7 +24,7 @@ export default function Filter({slides}:any){
                                     </div>
                                </Link>
                     })}
-                </div>
+                </div> */}
         </div>
         </>
     )
