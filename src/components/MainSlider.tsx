c85a7cb86c1c129d27cf3ce91d44506d96c86f91
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-16 18:13:29 +0100

    redisgn buttons

--- src/components/MainSlider.tsx
+++ src/components/MainSlider.tsx
@@ -45,11 +45,11 @@ export default function MainSlider({slides}:any){
                                         src={process.env.REACT_APP_MOVIE_IMAGE+slide.backdrop_path} alt="mainslider"/>
                                     </Link>                
                                     <div className="flex items-center justify-between ">
-                                        <MdChevronLeft size={60} onClick={()=>prevslide()} className='ml-[-20px] md:mt-[-37vw] mt-[-43vw] scrollBtn'/>
-                                        <MdChevronRight size={60} onClick={()=>nextslide()} className='mr-[-20px] md:mt-[-37vw] mt-[-43vw] scrollBtn'/>
+                                        <MdChevronLeft onClick={()=>prevslide()} className='text-[40px] sm:text-[55px] md:text-[65px] md:ml-[-20px] md:mt-[-37vw] mt-[-43vw] scrollBtn'/>
+                                        <MdChevronRight onClick={()=>nextslide()} className='text-[40px] sm:text-[55px] md:text-[65px] md:mr-[-20px] md:mt-[-37vw] mt-[-43vw] scrollBtn'/>
                                     </div>
 
-                                    <div className="flex px-3 sm:px-7 pb-6 sm:pb-7 mt-[-38px] sm:mt-[-60px] items-end text-txtPrimary bg-bgDark">
+                                    <div className="flex px-3 sm:px-7 pb-6 sm:pb-9 mt-[-38px] sm:mt-[-60px] items-end text-txtPrimary bg-bgDark">
                                         <div className="w-[70px] min-w-[70px] h-[100px] sm:w-[90px] sm:h-[140px] md:w-[100px] md:h-[150px] border-[5px] sm:border-[8px] border-bgDark">
                                             <img className="object-cover w-[100%] h-[100%]" 
                                             src={process.env.REACT_APP_MOVIE_IMAGE+slide.poster_path} alt="mainslider"/>
