53a9448838b3a97206203effa4040d6c972258df
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-16 22:34:13 +0100

    almost all api

--- src/components/Listings.tsx
+++ src/components/Listings.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { fetchMovies } from '../Redux/movies';
 import { UseAppDispatch } from '../Redux/store';
 
-export default function Listings({slides}:any){
+export default function Listings({slides,heading}:any){
     const id=''+Math.random();
     const dispatch=UseAppDispatch();
 
@@ -22,16 +22,16 @@ export default function Listings({slides}:any){
         <div className="mr-0 md:mr-[30px] pt-8 pb-0  sm:py-8">
             <div className="border-l-8 pl-3 border-brTertiary">
                 <p onClick={()=>dispatch(fetchMovies())} className="font-semibold md:font-bold text-2xl md:text-3xl pl-0 md:pl-3 text-txtPrimary">
-                    Top Box Office
+                    {heading}
                 </p>   
             </div>             
         <div className="flex justify-between items-center">
         <MdChevronLeft size={60} onClick={()=>prevslide()} className='mr-[-40px] scrollBtn hidden sm:block'/>
         <div id={`${id}`} className="w-[100%] flex justify-start items-center scrollbar-hide overflow-x-scroll py-4 scroll-smooth">
-            {slides.map((slide:string,i:number):any=>{
+            {slides.map((slide:any,i:number):any=>{
                 return  <Link to='/' key={i}  className='flex duration-300 w-[290px] min-w-[290px] sm:w-[350px] mx-3 sm:min-w-[350px]'>
                 <div className='w-[100px] h-28 sm:h-[140px]'>
-                    <img src={slide} alt='listings' className='object-cover w-full h-full'/>
+                    <img src={process.env.REACT_APP_MOVIE_IMAGE+slide.poster_path} alt='listings' className='object-cover w-full h-full'/>
                 </div>
                 <div className='flex-1 px-3 text-txtPrimary'>
                     <p className='font-semibold md:font-bold  line-clamp-3'>Oscar Preview: Maybe the multiverse Will Give Us the Best Picture Winner We Deserve</p>
