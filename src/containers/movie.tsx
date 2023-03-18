24e8ac5dc6ce1dd923b6411f734fbb458d4bfd39
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-18 23:28:41 +0100

    movie header

--- src/containers/movie.tsx
+++ src/containers/movie.tsx
@@ -8,24 +8,24 @@ export default function Movie(){
         <>
         <div className="text-primary">                
             <div className='md:ml-[120px] ml-0 sm:px-5'>
-                <div className="bgImageGrad px-36 text-txtPrimary">
-                    <div className="py-7">
-                        <p>Home/TV Shows/Daisy Jones & The Six</p>
+                <div className="bgImageGrad text-txtPrimary">
+                    <div className="py-7 px-20">
+                        <p className="font-semibold text-txtPrimary opacity-90">Home | TV Shows | Daisy Jones & The Six</p>
                     </div>
-                    <div className="py-40">
+                    <div className="py-40 px-36 pb-20">
                         <div>
-                            <p>WATCH</p>
-                            <p>Daisy Jones & The Six</p>
+                            <p className="text-brSecondary font-semibold text-lg">WATCH</p>
+                            <p className="font-bold text-5xl py-4">Daisy Jones & The Six</p>
                         </div>
-                        <div>
-                            <p>Drama</p>
-                            <p>Comedy</p>
-                            <p>Romance</p>
+                        <div className="flex items-center py-4">
+                            <p className="p-2 mx-2 text-sm rounded-full border border-txtPrimary bg-bgPrimary w-[100px] flex justify-center items-center">Drama</p>
+                            <p className="p-2 mx-2 text-sm rounded-full border border-txtPrimary bg-bgPrimary w-[100px] flex justify-center items-center">Comedy</p>
+                            <p className="p-2 mx-2 text-sm rounded-full border border-txtPrimary bg-bgPrimary w-[100px] flex justify-center items-center">Romance</p>
                         </div>
-                        <div>
-                            <button><FaPlay/>Watch Now</button>
-                            <button><FaHeart/>Add to Favourites</button>
-                            <button><FaShare/></button>
+                        <div className="flex items-center py-4">
+                            <button className="flex rounded-lg items-center bg-brPrimary p-5 mx-2 text-sm w-[200px] justify-center"><FaPlay/>Watch Now</button>
+                            <button className="flex rounded-lg items-center bg-brPrimary p-5 mx-2 text-sm w-[200px] justify-center"><FaHeart/>Add to Favourites</button>
+                            <button className="flex rounded-lg items-center bg-brPrimary p-5 mx-2 text-sm"><FaShare/></button>
                         </div>
                     </div>                    
                 </div>
