932891205979ac0133b0a7e71c2a9b3f18a0193a
Steven Joseph <stevolisisjoseph@gmail.com>
2023-03-18 23:55:37 +0100

    responsive movie header

--- src/containers/movie.tsx
+++ src/containers/movie.tsx
@@ -8,14 +8,14 @@ export default function Movie(){
         <>
         <div className="text-primary">                
             <div className='md:ml-[120px] ml-0 sm:px-5'>
-                <div className="bgImageGrad text-txtPrimary">
-                    <div className="py-7 px-20">
-                        <p className="font-semibold text-txtPrimary opacity-90">Home | TV Shows | Daisy Jones & The Six</p>
+                <div className="bgImageGrad text-txtPrimary px-5 sm:px-0">
+                    <div className="py-5 sm:py-7 sm:px-16 md:px-20">
+                        <p className="text-sm sm:text-base font-semibold text-txtPrimary opacity-90">Home | TV Shows | Daisy Jones & The Six</p>
                     </div>
-                    <div className="py-40 px-36 pb-20">
+                    <div className="py-[6rem] pb-10 sm:py-28 sm:px-20 sm:pb-10 md:py-40 md:px-36 md:pb-20">
                         <div>
-                            <p className="text-brSecondary font-semibold text-lg">WATCH</p>
-                            <p className="font-bold text-5xl py-4">Daisy Jones & The Six</p>
+                            <p className="text-brSecondary font-semibold text-sm sm:text-[17px] md:text-lg">WATCH</p>
+                            <p className="font-bold text-3xl sm:text-4xl md:text-5xl ">Daisy Jones & The Six</p>
                         </div>
                         <div className="flex items-center py-4">
                             <p className="p-2 mx-2 text-sm rounded-full border border-txtPrimary bg-bgPrimary w-[100px] flex justify-center items-center">Drama</p>
@@ -23,9 +23,9 @@ export default function Movie(){
                             <p className="p-2 mx-2 text-sm rounded-full border border-txtPrimary bg-bgPrimary w-[100px] flex justify-center items-center">Romance</p>
                         </div>
                         <div className="flex items-center py-4">
-                            <button className="flex rounded-lg items-center bg-brPrimary p-5 mx-2 text-sm w-[200px] justify-center"><FaPlay/>Watch Now</button>
-                            <button className="flex rounded-lg items-center bg-brPrimary p-5 mx-2 text-sm w-[200px] justify-center"><FaHeart/>Add to Favourites</button>
-                            <button className="flex rounded-lg items-center bg-brPrimary p-5 mx-2 text-sm"><FaShare/></button>
+                            <button className="flex rounded-lg items-center bg-brPrimary p-5 mx-2 text-sm w-[200px] justify-center"><FaPlay size={15} className="mr-2"/>Watch Now</button>
+                            <button className="flex rounded-lg items-center bg-brPrimary p-5 mx-2 text-sm w-[200px] justify-center"><FaHeart size={15} className="mr-2"/>Add to Favourites</button>
+                            <button className="flex rounded-lg items-center bg-brPrimary p-5 mx-2 text-sm"><FaShare size={15}/></button>
                         </div>
                     </div>                    
                 </div>
