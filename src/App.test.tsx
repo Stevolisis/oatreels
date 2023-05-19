e8a1057e01fb0dff2371b8d47494e80e4caf49f8
Steven Joseph <stevolisisjoseph@gmail.com>
2023-05-19 13:27:00 +0100

    test implementation

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+// math.test.js
+
+function sum(a:number, b:number):number {
+    return a + b;
+  }
+  
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(33);
+});
+  
+export {};
